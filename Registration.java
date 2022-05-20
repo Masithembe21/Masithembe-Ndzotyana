38993a43338f25eea0f62b24cb07be2bb2a19219
Masithembe21 <105933433+Masithembe21@users.noreply.github.com>
2022-05-20 17:27:15 +0200

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Registration;
+
+/**
+ *
+ * @author Masithembe Ndzotyana
+ */
+public class Registration {
+    
+      public static void main(String[] args)throws Exception {
+        // TODO code application logic here
+        MyFrame f = new MyFrame();
+
+    }
+    
+}
